79a7792f0b09fc3ebef9a76da365ea5f2cf03500
ardeshir1988 <ardeshir.ahouri@gmail.com>
2019-06-03 12:53:25 +0430

    empty basket handled

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -2,14 +2,11 @@ import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import Paper from '@material-ui/core/Paper';
 import BasketProduct from "./BasketProduct";
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import Button from '@material-ui/core/Button';
 import Badge from '@material-ui/core/Badge';
 import {CartContext} from "./CartContext";
-
+import Snackbar from "@material-ui/core/Snackbar";
 
 const styles = {
     list: {
@@ -20,6 +17,12 @@ const styles = {
     }
 };
 
+const Snack = withStyles({
+    root: {
+            direction:"rtl"
+    }
+})(Snackbar);
+
 class Basket extends React.Component {
 
 
@@ -27,6 +30,7 @@ class Basket extends React.Component {
         super(props);
         this.state = {
             status:false,
+            msg:false
         };
     }
     toggleDrawer = (status) => () => {
@@ -40,7 +44,17 @@ class Basket extends React.Component {
         window.location.href=(path);
     }
 
+    handleClose=()=>{
+        this.setState({msg:false})
+    };
 
+    handleOpen=()=>{
+        this.setState({msg:true})
+    };
+    closeDrawer=()=>{
+        this.toggleDrawer(false);
+        return false
+    };
     render() {
         console.log("=======RenderBasket");
         const quantity='تعداد';
@@ -55,10 +69,19 @@ class Basket extends React.Component {
             <CartContext.Consumer>
                 {cart=>(
             <div>
+                <Snack
+                anchorOrigin={{
+                    vertical: 'center',
+                    horizontal: 'center',
+                }}
+                open={this.state.msg}
+                autoHideDuration={2000}
+                onClose={this.handleClose}
+                message={<h4>سبد شما خالی است</h4>}/>
                     <Badge badgeContent={cart.items.map(c=>c.quantity).reduce((partial_sum, a) => partial_sum + a,0)} color="secondary" >
-                    <ShoppingCartIcon style={{color:"white"}} onClick={this.toggleDrawer(true)}/>
+                    <ShoppingCartIcon  style={{color:"white"}} onClick={(cart.items.map(c=>c.quantity).reduce((partial_sum, a) => partial_sum + a,0)>=1)?this.toggleDrawer(true):this.handleOpen}/>
                     </Badge>
-                <Drawer anchor="left" open={this.state.status} onClose={this.toggleDrawer( false)}>
+                <Drawer anchor="left" open={(cart.items.length>0)?this.state.status:this.closeDrawer()}  onClose={this.toggleDrawer( false)}>
                     <div
                         tabIndex={0}
                         role="button">
@@ -93,12 +116,12 @@ class Basket extends React.Component {
                                 </div>
 
                                 <div className="basketDetail">
-                                    <div className="data">{cart.items.map(c=>c.quantity*c.price).reduce((partial_sum, a) => partial_sum + a,0)-
+                                    <div className="data" style={{color:"limegreen"}}>-{cart.items.map(c=>c.quantity*c.price).reduce((partial_sum, a) => partial_sum + a,0)-
                                     cart.items.map(c=>c.quantity*c.priceDiscount).reduce((partial_sum, a) => partial_sum + a,0)}</div>
                                     <div className="title">{discount}</div>
                                 </div>
                                 <div className="basketDetail">
-                                    <div className="data">{(cart.items.map(c=>c.quantity*c.price).reduce((partial_sum, a) => partial_sum + a,0)>=cart.threshold) ? 0 : cart.deliveryCost} </div>
+                                    <div className="data" style={{color:"red"}}>+{(cart.items.map(c=>c.quantity*c.price).reduce((partial_sum, a) => partial_sum + a,0)>=cart.threshold) ? 0 : cart.deliveryCost} </div>
                                     <div className="title">{deliveryFee}</div>
                                 </div>
 
